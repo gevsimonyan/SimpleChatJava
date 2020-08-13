c01dd2273d33529201e26aaa8711167260660cf3
Gev <gevorksik1995@gmail.com>
2020-08-13 16:58:17 +0300

    Simple Chat

---
+++ Network/src/am/gevdef/network/TCPConnectionListener.java
@@ -0,0 +1,13 @@
+package am.gevdef.network;
+
+public interface TCPConnectionListener {
+
+    void onConnectionReady(TCPConnection tcpConnection);
+
+    void onReceiveString(TCPConnection tcpConnection, String value);
+
+    void onDisconnect(TCPConnection tcpConnection);
+
+    void onException(TCPConnection tcpConnection, Exception e);
+
+}
